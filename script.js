3c8bd57f850dbf9b987016366f1d8578bf6dbf2e
Sukanya Gurav <sukanyagurav6@gmail.com>
2023-01-24 22:14:54 +0530

    not completed

--- script.js
+++ script.js
@@ -4,65 +4,123 @@ const lastName = document.getElementById('lastName')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const  inputs = document.querySelectorAll('input')
-const showBtn = document.querySelector('.fa-eye-slash')
-const hideBtn = document.querySelector('.fa-eye')
+const showBtn = document.querySelector('#showPassword')
+
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     vaildateForm();
 })
+let verifyCount=0;
+function checkIfEmpty(inputValue,input,placeholder){
+    if(inputValue === ''){
+        setError(input,`${placeholder} cannot be empty`)
+      
+        return true
+    }
+    return false
+}
 
 function vaildateForm(){
     let firstNameValue=firstName.value.trim();
     let lastNameValue=lastName.value.trim();
     let emailValue=email.value.trim();
     let pwdValue=password.value.trim();
-    let re=/[!@#$%^&*]/g
-    if(firstNameValue ===''){
-        setError(firstName,'First name cannot be empty')
+    if(!checkIfEmpty(firstNameValue,firstName,'First Name')){
+        setSuccess(firstName)
+      
     }
-   
-    if(lastNameValue===''){
-        setError(lastName,'Last name cannot be empty')
+    if(!checkIfEmpty(lastNameValue,lastName,'Last Name') ){
+        setSuccess(lastName)
+      
     }
+    checkIfEmpty(pwdValue,password,'Password') || checkPassword(pwdValue);
+    checkIfEmpty(emailValue,email,'Email') || checkEmail(emailValue);
+    registerSuccessful()
+}
 
-    if(pwdValue ===''){
-        setError(password,'Password cannot be empty')
+function registerSuccessful(){
+    var time;
+    const formControl = document.getElementsByClassName('form-control')
+    var count=0;
+    for(var i=0;i< formControl.length;i++){
+        if(formControl[i].className ==='form-control success'){
+           count++;
+        }
+    }
+   if(count === 4){
+
+    const confetti =document.querySelector('.confetti-container');
+    time= setTimeout(()=>{
+        confetti.style.visibility = 'visible';
+       
+    },1000)
+    
+    setTimeout(function( ) { 
+         clearInterval( time );
+         firstName.value='';
+         lastName.value='';
+         email.value='';
+         password.value='';
+         confetti.style.visibility = 'hidden';
+        
+         for(var i=0;i< formControl.length;i++){
+            formControl[i].classList.remove('success')
+        }
+     }, 3000);
+   }
+  }
+function checkPassword(passwordValue){
+    let re=/[!@#$%_^&*]/g
+    if(passwordValue.length < 8){
+        setError(password,'Passwords should contain at lease 8 characters')
+
+    }
+    else if(!passwordValue.match(re)){
+        setError(password,'Passwords should contain special characters')
+    }
+    else if(!passwordValue.match(/\d/)){
+        setError(password,'Passwords should contain numbers')
     }
     else{
-        
-        if(pwdValue.length < 8){
-            setError(password,'Passwords should contain at lease 8 characters')
-    
-        }
-        else if(!pwdValue.match(re)){
-            setError(password,'Passwords should contain special characters')
-        }
-        else if(!pwdValue.match(/[0-9]/g)){
-            setError(password,'Passwords should contain numbers')
-        }
+        setSuccess(password)
+      
     }
+  }
+function checkEmail(emailValue){
+    let regex= /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,20})(\.[a-z]{2,8})?$/
+   
+     if(!emailValue.includes('@')){
+        setError(email,'Please include @ in the email address')
+    }
+    else if(!regex.test(emailValue)){
+        setError(email,'Invalid email address')
+    }
+    else{
+        setSuccess(email)
+        
+    }
+}
+function setSuccess(input){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+    if(formControl.classList.contains('error')){
+        formControl.classList.remove('error')
+        
+    } 
+    formControl.classList.add('success')
+	small.innerText = '';
 }
 function setError(input, message){
     const formControl = input.parentElement;
+  
     const small = formControl.querySelector('small')
+
     formControl.className = 'form-control error';
 	small.innerText = message;
 }
 showBtn.addEventListener('click',()=>{
-    if(password.type === 'password'){
-        password.type='text';
-        hideBtn.classList.add('show')
-        showBtn.classList.add('hide')
-        // hideBtn.style.visibility ='visible'
-        // showBtn.style.visibility = 'hidden'
-    }
-    else{
-        password.type='password';
-        hideBtn.classList.remove('show')
-        showBtn.classList.remove('hide')
-    }
+    const type = password.getAttribute("type") === "password" ? 'text' : "password";
+    password.setAttribute("type",type);
+    showBtn.classList.toggle("fa-eye"); 
+
 })
-// hideBtn.addEventListener('click',()=>{
-//     hideBtn.style.visibility ='hidden'
-//     showBtn.style.visibility = 'visible'
-// })
